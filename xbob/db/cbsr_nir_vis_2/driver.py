eb5bb903015e17bac7229203fec4a705f0b101c3
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2014-10-27 14:18:45 +0100

    Renamed the xbob directory

---
+++ xbob/db/cbsr_nir_vis_2/driver.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# Laurent El Shafey <laurent.el-shafey@idiap.ch>
+# Fri Aug 23 16:51:41 CEST 2013
+#
+# Copyright (C) 2011-2014 Idiap Research Institute, Martigny, Switzerland
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Commands the CASIA NIR-VIS 2.0 database can respond to.
+"""
+
+import os
+import sys
+from xbob.db.base.driver import Interface as BaseInterface
+
+def dumplist(args):
+  """Dumps lists of files based on your criteria"""
+
+  from .query import Database
+  db = Database()
+
+  r = db.objects(
+      purposes=args.purpose,
+      groups=args.group,
+  )
+
+  output = sys.stdout
+  if args.selftest:
+    from xbob.db.base.utils import null
+    output = null()
+
+  for f in r:
+    output.write('%s\n' % f.make_path(directory=args.directory,extension=args.extension))
+
+  return 0
+
+def checkfiles(args):
+  """Checks existence of files based on your criteria"""
+
+  from .query import Database
+  db = Database()
+
+  r = db.objects()
+
+  # go through all files, check if they are available on the filesystem
+  good = []
+  bad = []
+  for f in r:
+    if os.path.exists(f.make_path(args.directory, args.extension)): good.append(f)
+    else: bad.append(f)
+
+  # report
+  output = sys.stdout
+  if args.selftest:
+    from xbob.db.base.utils import null
+    output = null()
+
+  if bad:
+    for f in bad:
+      output.write('Cannot find file "%s"\n' % f.make_path(args.directory, args.extension))
+    output.write('%d files (out of %d) were not found at "%s"\n' % \
+        (len(bad), len(r), args.directory))
+
+  return 0
+
+class Interface(BaseInterface):
+
+  def name(self):
+    return 'voxforge'
+
+  def version(self):
+    import pkg_resources # part of setuptools
+    return pkg_resources.require('xbob.db.%s' % self.name())[0].version
+
+  def files(self):
+    return ()
+
+  def type(self):
+    return 'text'
+
+  def add_commands(self, parser):
+
+    from . import __doc__ as docs
+
+    subparsers = self.setup_parser(parser,
+        "Voxforge database", docs)
+
+    import argparse
+
+    # the "dumplist" action
+    parser = subparsers.add_parser('dumplist', help=dumplist.__doc__)
+    parser.add_argument('-d', '--directory', default='', help="if given, this path will be prepended to every entry returned.")
+    parser.add_argument('-e', '--extension', default='', help="if given, this extension will be appended to every entry returned.")
+    parser.add_argument('-u', '--purpose', help="if given, this value will limit the output files to those designed for the given purposes.", choices=('enrol', 'probe', ''))
+    parser.add_argument('-g', '--group', help="if given, this value will limit the output files to those belonging to a particular protocolar group.", choices=('dev', 'eval', 'world', 'optional_world_1', 'optional_world_2', ''))
+    parser.add_argument('--self-test', dest="selftest", action='store_true', help=argparse.SUPPRESS)
+    parser.set_defaults(func=dumplist) #action
+
+    # the "checkfiles" action
+    parser = subparsers.add_parser('checkfiles', help=checkfiles.__doc__)
+    parser.add_argument('-l', '--list-directory', required=True, help="The directory which contains the file lists.")
+    parser.add_argument('-d', '--directory', dest="directory", default='', help="if given, this path will be prepended to every entry returned.")
+    parser.add_argument('-e', '--extension', dest="extension", default='', help="if given, this extension will be appended to every entry returned.")
+    parser.add_argument('--self-test', dest="selftest", action='store_true', help=argparse.SUPPRESS)
+
+    parser.set_defaults(func=checkfiles) #action
