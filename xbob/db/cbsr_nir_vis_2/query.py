2a10a93ed5e65f83933b329384db16e4a9f303b1
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2014-10-29 19:48:49 +0100

    Added the annotations method

--- xbob/db/cbsr_nir_vis_2/query.py
+++ xbob/db/cbsr_nir_vis_2/query.py
@@ -24,14 +24,17 @@ class Database(xbob.db.verification.filelist.Database):
   this class defines a simple protocol for training, dev and and by splitting the audio files of the database in three main parts.
   """
 
-  def __init__(self, original_directory = None, original_extension = None, annotation_directory=None):
+  def __init__(self, original_directory = None, original_extension = None, annotation_directory=None, annotation_type="eyecenter", annotation_extension=".pos"):
     # call base class constructor
     from pkg_resources import resource_filename
     lists = resource_filename(__name__, 'lists')
     #xbob.db.verification.filelist.Database.__init__(self, lists, original_directory=original_directory, original_extension = original_extension, annotation_directory=annotation_directory)
     xbob.db.verification.filelist.Database.__init__(self, lists, original_directory)
 
+    self.m_annotation_directory = annotation_directory
+    self.m_annotation_extension = annotation_extension
     self.original_extension = original_extension
+    self.m_annotation_type = annotation_type
 
   def tobjects(self, protocol=None, model_ids=None, groups=None):
     #No TObjects    
@@ -41,4 +44,21 @@ class Database(xbob.db.verification.filelist.Database):
   def zobjects(self, protocol=None, groups=None):
     #No TObjects    
     return []
+
+
+  def annotations(self, file_id):
+    """Reads the annotations for the given file id from file and returns them in a dictionary.
+    If you don't have a copy of the annotation files, you can download them under http://www.idiap.ch/resource/biometric.
+    Keyword parameters:
+    file_id
+    The ID of the file for which the annotations should be read.
+    Return value
+    The annotations as a dictionary: {'reye':(re_y,re_x), 'leye':(le_y,le_x)}
+    """
+    if self.m_annotation_directory is None:
+      return None
+    # since the file id is equal to the file name, we can simply use it
+    annotation_file = os.path.join(self.m_annotation_directory, file_id + self.m_annotation_extension)
+    # return the annotations as read from file
+    return xbob.db.verification.utils.read_annotation_file(annotation_file, self.m_annotation_type)
 
