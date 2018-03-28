6c6e01800d5877eb7cfa770adcfe52491d295c9d
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-03-28 08:56:24 +0200

    [sphinx] Fixed warnings

--- bob/db/cbsr_nir_vis_2/__init__.py
+++ bob/db/cbsr_nir_vis_2/__init__.py
@@ -41,5 +41,25 @@ def get_config():
 
   return retval.strip()
 
+
+# gets sphinx autodoc done right - don't remove it
+def __appropriate__(*args):
+  """Says object was actually declared here, an not on the import module.
+
+  Parameters:
+
+    *args: An iterable of objects to modify
+
+  Resolves `Sphinx referencing issues
+  <https://github.com/sphinx-doc/sphinx/issues/3048>`
+  """
+
+  for obj in args: obj.__module__ = __name__
+
+__appropriate__(
+    Database,
+    )
+
+
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
